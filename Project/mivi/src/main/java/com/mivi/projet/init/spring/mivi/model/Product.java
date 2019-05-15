be73f93422dac0ec0c803d6455bd14aa14f5125d
Lea Kim <mickael.vilalard@hotmail.fr>
2019-05-15 22:00:50 +0200

    Project initialisation

---
+++ Project/mivi/src/main/java/com/mivi/projet/init/spring/mivi/model/Product.java
@@ -0,0 +1,6 @@
+package com.mivi.projet.init.spring.mivi.model;
+
+public class Product {
+
+
+}
---
+++ Project/mivi/src/test/java/com/mivi/projet/init/spring/mivi/MiviApplicationTests.java
@@ -0,0 +1,16 @@
+package com.mivi.projet.init.spring.mivi;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MiviApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
